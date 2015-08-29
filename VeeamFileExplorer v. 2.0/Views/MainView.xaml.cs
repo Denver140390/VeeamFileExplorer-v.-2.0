6852d007e00ba0f4ed87dd3ada3da8f2cbe84422
Denver140390 <freeman140390@gmaiil.com>
2015-08-29 13:27:54 +0300

    Commiting MainView.xaml For some reason it was marked as untracked file.

---
+++ VeeamFileExplorer v. 2.0/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace VeeamFileExplorer_v._2._0
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
