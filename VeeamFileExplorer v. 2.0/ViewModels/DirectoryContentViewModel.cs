9ee9b4287c073de50c184cfbbcf2cf4c032a89ce
Denis Minakov <minakov-da@radar-dev.net>
2015-09-07 17:30:10 +0400

    Trying to cancel the task if a new directory content loads...

--- VeeamFileExplorer v. 2.0/ViewModels/DirectoryContentViewModel.cs
+++ VeeamFileExplorer v. 2.0/ViewModels/DirectoryContentViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -21,6 +22,9 @@ namespace VeeamFileExplorer_v._2._0.ViewModels
 //            }
 //        }
 
+        private Task _task;
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
         public string Path { get; private set; }
 
         public ObservableCollection<IFileSystemEntityViewModel> Content { get; } = new ObservableCollection<IFileSystemEntityViewModel>();
@@ -31,29 +35,51 @@ namespace VeeamFileExplorer_v._2._0.ViewModels
         {
             NavigationViewModel.Navigating += OnNavigating;
         }
-
+        
         public async void LoadContentAsync(string path)
         {
+            if (_task != null && !_task.IsCompleted)
+            {
+                _cancellationTokenSource.Cancel();
+                //_cancellationTokenSource = new CancellationTokenSource();
+            }
+
+            //_task = LoadContent(path);
             Content.Clear();
             Path = path;
             NavigationViewModel.Add(Path);
 
-            await Task.Run(() =>
+            //_cancellationTokenSource = new CancellationTokenSource();
+            _task = Task.Run(() => LoadContent(path), _cancellationTokenSource.Token);
+            await _task;
+        }
+
+        private void LoadContent(string path)
+        {
+            try
             {
                 var directoryPaths = Directory.GetDirectories(path);
                 foreach (var directoryPath in directoryPaths)
                 {
+                    _cancellationTokenSource.Token.ThrowIfCancellationRequested();
                     var directory = new DirectoryViewModel(directoryPath);
                     Application.Current.Dispatcher.Invoke(() => Content.Add(directory));
+                    _cancellationTokenSource.Token.ThrowIfCancellationRequested();
                 }
 
                 var filePaths = Directory.GetFiles(path);
                 foreach (var filePath in filePaths)
                 {
+                    _cancellationTokenSource.Token.ThrowIfCancellationRequested();
                     var file = new FileViewModel(filePath);
                     Application.Current.Dispatcher.Invoke(() => Content.Add(file));
+                    _cancellationTokenSource.Token.ThrowIfCancellationRequested();
                 }
-            });
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
 
         private void OnNavigating(object sender, NavigationEventArgs args)
